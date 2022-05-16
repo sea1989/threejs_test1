63b3b40162741c8f3932fb0cef80f96ce4db4b25
Victor <vicvilkov@yandex.ru>
2022-05-16 23:58:18 +0300

    add music

---
+++ src/classes/SoundReactor.js
@@ -0,0 +1,51 @@
+import RAF from '../utils/RAF'
+
+class SoundReactor {
+
+    constructor(audioUrl) {
+        this.ctx
+        this.audio
+        this.audioSource
+        this.analyser
+        this.fdata
+        this.url = audioUrl
+
+        this.bind()
+    }
+
+    init() {
+        this.ctx = new AudioContext();
+        this.audio = new Audio(this.url);
+        this.audioSource = this.ctx.createMediaElementSource(this.audio);
+        this.analyser = this.ctx.createAnalyser();
+        this.analyser.smoothingTimeConstant = 0.8
+
+        this.audioSource.connect(this.analyser);
+        this.audioSource.connect(this.ctx.destination);
+        this.fdata = new Uint8Array(this.analyser.frequencyBinCount);
+    }
+
+    play() {
+        this.audio.play()
+        RAF.subscribe('audioReactorUpdate', this.update)
+    }
+
+    pause() {
+        this.audio.pause()
+        RAF.subscribe('audioReactorUpdate')
+    }
+
+    update() {
+        this.analyser.getByteFrequencyData(this.fdata);
+
+    }
+
+    bind() {
+        this.update = this.update.bind(this)
+        this.init = this.init.bind(this)
+    }
+
+}
+
+const _instance = new SoundReactor("assets/nice-day.mp3")
+export default _instance;
--- src/shaders/spectrum.frag
+++ src/shaders/spectrum.frag
@@ -7,12 +7,19 @@ varying vec2 vMatCapUV;
 
 uniform sampler2D uMatCap;
 uniform float uSpecterSize;
+uniform float uWaveBorder;
+uniform vec3 uBorderColor;
 uniform float uTime;
 
 void main() {
+
     float n3 = snoise3(vec3(vPosition.xz*5.,uTime*0.01))*.5;
 
     float w = sin(vPosition.y*5. - uTime*0.1);
+
+    float borderMask = step(w, n3-uSpecterSize);
+    borderMask -= step(w, n3-(uSpecterSize+uWaveBorder));
+    vec4 borderOut = vec4(uBorderColor*borderMask, borderMask);
 
     float mcMask = step(w, n3-uSpecterSize);
 
@@ -24,8 +31,8 @@ void main() {
     opMask+=.5;
     vec4 opMaskOut = vec4(1.,1., 1., opMask);
 
-    vec4 col = matCapOut;
+    vec4 col = matCapOut + borderOut;
     col*=opMaskOut;
 
-    gl_FragColor = vec4(vec3(borderMask), 1.);
+    gl_FragColor = vec4(col);
 }
